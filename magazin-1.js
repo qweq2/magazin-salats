89c6a6826fa0410cb6822313cd0406d2f135b5de
qweq2 <134150891+qweq2@users.noreply.github.com>
2024-05-25 19:25:27 +0300

    Add files via upload

---
+++ magazin-1.js
@@ -0,0 +1,30 @@
+const body = document.body;
+const btn = document.querySelector(".cart");
+const btnClose = document.querySelector("#btn_close");
+const modal = document.querySelector("#modal");
+
+
+btn.addEventListener("click", btnHandler);
+btnClose.addEventListener("click", btnCloseHandler);
+
+
+function btnHandler(e) {
+  e.preventDefault();
+  body.classList.add("lock");
+  modal.classList.add("modal--open");
+}
+
+
+function btnCloseHandler(e) {
+  e.preventDefault();
+  body.classList.remove("lock");
+  modal.classList.remove("modal--open");
+}
+
+
+window.addEventListener("click", function (e) {
+  if (e.target == modal) {
+    body.classList.remove("lock");
+    modal.classList.remove("modal--open");
+  }
+});
