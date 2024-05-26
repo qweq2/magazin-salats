848fad7741b4f2b696eb2aa138071a8d003e1292
qweq2 <134150891+qweq2@users.noreply.github.com>
2024-05-26 17:28:36 +0300

    Add files via upload

--- magazin-1.js
+++ magazin-1.js
@@ -1,30 +1,32 @@
-const body = document.body;
-const btn = document.querySelector(".cart");
-const btnClose = document.querySelector("#btn_close");
-const modal = document.querySelector("#modal");
+// const body = document.body;
+// const btn = document.querySelector(".cart");
+// const btnClose = document.querySelector("#btn_close");
+// const modal = document.querySelector("#modal");
 
 
-btn.addEventListener("click", btnHandler);
-btnClose.addEventListener("click", btnCloseHandler);
+// btn.addEventListener("click", btnHandler);
+// btnClose.addEventListener("click", btnCloseHandler);
 
 
-function btnHandler(e) {
-  e.preventDefault();
-  body.classList.add("lock");
-  modal.classList.add("modal--open");
-}
+// function btnHandler(e) {
+//   e.preventDefault();
+//   body.classList.add("lock");
+//   modal.classList.add("modal--open");
+// }
 
 
-function btnCloseHandler(e) {
-  e.preventDefault();
-  body.classList.remove("lock");
-  modal.classList.remove("modal--open");
-}
+// function btnCloseHandler(e) {
+//   e.preventDefault();
+//   body.classList.remove("lock");
+//   modal.classList.remove("modal--open");
+// }
 
 
-window.addEventListener("click", function (e) {
-  if (e.target == modal) {
-    body.classList.remove("lock");
-    modal.classList.remove("modal--open");
-  }
-});
+// window.addEventListener("click", function (e) {
+//   if (e.target == modal) {
+//     body.classList.remove("lock");
+//     modal.classList.remove("modal--open");
+//   }
+// });
+
+
